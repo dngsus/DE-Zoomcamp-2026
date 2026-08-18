e05ee692b3e3f5b3b620412ba05e98120bae6aa9
dngsus <152196081+dngsus@users.noreply.github.com>
2026-08-18 02:04:18 +0000

    20260818

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,3 +1,7 @@
+# import os
+# output_dir = "outputs"
+# os.makedirs(output_dir, exist_ok=True)
+
 import sys
 print("arguments", sys.argv)
 
@@ -8,5 +12,7 @@ import pandas as pd
 
 df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
 print(df.head())
+
+#df.to_parquet(f"{output_dir}/output_month{sys.argv[1]}.parquet")
 
 df.to_parquet(f"output_month_{sys.argv[1]}.parquet")
